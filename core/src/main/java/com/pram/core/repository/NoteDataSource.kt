0ec980dd5ea685b5d835bc4bef36c2c75def4fae
preepramdev <preepramdev@hmail.com>
2020-11-08 21:18:21 +0700

    Repository

---
+++ core/src/main/java/com/pram/core/repository/NoteDataSource.kt
@@ -0,0 +1,13 @@
+package com.pram.core.repository
+
+import com.pram.core.data.Note
+
+interface NoteDataSource {
+    suspend fun add(note: Note)
+
+    suspend fun get(id: Long): Note?
+
+    suspend fun getAll(): List<Note>
+
+    suspend fun remove(note: Note)
+}
