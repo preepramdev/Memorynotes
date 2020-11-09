16d5bc7e5aa629289607a862709590b206a92a37
preepramdev <preepramdev@hmail.com>
2020-11-10 00:26:24 +0700

    Room entities

---
+++ app/src/main/java/com/pram/memorynotes/framework/db/NoteEntity.kt
@@ -0,0 +1,24 @@
+package com.pram.memorynotes.framework.db
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.pram.core.data.Note
+
+@Entity(tableName = "note")
+data class NoteEntity(
+    var title: String,
+    var content: String,
+    @ColumnInfo(name = "creation_date")
+    var creationTime: Long,
+    @ColumnInfo(name = "update_time")
+    var updateTime: Long,
+    @PrimaryKey(autoGenerate = true)
+    var id: Long = 0L
+) {
+    companion object {
+        fun fromNote(note: Note) = NoteEntity(note.title, note.content, note.creationTime, note.updateTime)
+    }
+
+    fun toNote() = Note(title, content, creationTime, updateTime, id)
+}
