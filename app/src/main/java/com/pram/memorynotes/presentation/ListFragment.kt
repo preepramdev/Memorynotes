7e5e92936c246150cf271eb336c5c1afce1a6cb3
preepramdev <preepramdev@hmail.com>
2020-11-09 16:47:11 +0700

    Setup navigation

--- app/src/main/java/com/pram/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/pram/memorynotes/presentation/ListFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.pram.memorynotes.R
+import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
 
@@ -15,5 +17,18 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        addNote.setOnClickListener {
+            goToNoteDetails()
+        }
+    }
+
+    private fun goToNoteDetails(id: Long = 0L) {
+        val action = ListFragmentDirections.actionGoToNote(id)
+        Navigation.findNavController(notesListView).navigate(action)
     }
 }
