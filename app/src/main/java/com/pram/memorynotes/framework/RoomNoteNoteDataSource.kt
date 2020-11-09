e1a20b8e37d03092d139fef41471133aa28213d2
preepramdev <preepramdev@hmail.com>
2020-11-10 00:51:31 +0700

    Room data source

---
+++ app/src/main/java/com/pram/memorynotes/framework/RoomNoteNoteDataSource.kt
@@ -0,0 +1,20 @@
+package com.pram.memorynotes.framework
+
+import android.content.Context
+import com.pram.core.data.Note
+import com.pram.core.repository.NoteDataSource
+import com.pram.memorynotes.framework.db.DatabaseService
+import com.pram.memorynotes.framework.db.NoteEntity
+
+class RoomNoteNoteDataSource(context: Context): NoteDataSource {
+    val noteDao = DatabaseService.getInstance(context).noteDao()
+
+
+    override suspend fun add(note: Note) = noteDao.addNoteEntity(NoteEntity.fromNote(note))
+
+    override suspend fun get(id: Long): Note? = noteDao.getNoteEntity(id)?.toNote()
+
+    override suspend fun getAll(): List<Note> = noteDao.getAllNoteEntity().map { it.toNote() }
+
+    override suspend fun remove(note: Note) = noteDao.deleteNoteEntity(NoteEntity.fromNote(note))
+}
