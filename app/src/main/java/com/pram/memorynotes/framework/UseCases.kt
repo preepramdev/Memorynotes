bb9151c1e7e5f7c252ed243000a2d299ecdebf7d
preepramdev <preepramdev@hmail.com>
2020-11-10 00:54:40 +0700

    UseCases

---
+++ app/src/main/java/com/pram/memorynotes/framework/UseCases.kt
@@ -0,0 +1,13 @@
+package com.pram.memorynotes.framework
+
+import com.pram.core.usecase.AddNote
+import com.pram.core.usecase.GetAllNotes
+import com.pram.core.usecase.GetNote
+import com.pram.core.usecase.RemoveNote
+
+data class UseCases(
+    val addNote: AddNote,
+    val getAllNotes: GetAllNotes,
+    val getNote: GetNote,
+    val removeNote: RemoveNote
+)
