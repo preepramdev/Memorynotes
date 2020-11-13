49fc25e888d1ed34c8b1cdd0cfcbba393f5b1bbf
preepramdev <preepramdev@gmail.com>
2020-11-13 21:51:25 +0700

    ListFragment functionality

--- app/src/main/java/com/pram/memorynotes/presentation/ListFragment.kt
+++ app/src/main/java/com/pram/memorynotes/presentation/ListFragment.kt
@@ -5,11 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.pram.memorynotes.R
+import com.pram.memorynotes.framework.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 
 class ListFragment : Fragment() {
+
+    private val notesListAdapter = NotesListAdapter(arrayListOf())
+    private lateinit var viewModel: ListViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +29,31 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        notesListView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = notesListAdapter
+        }
+
         addNote.setOnClickListener {
             goToNoteDetails()
         }
+
+        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.notes.observe(viewLifecycleOwner, Observer { noteList ->
+            loadingView.visibility = View.GONE
+            notesListView.visibility = View.VISIBLE
+            notesListAdapter.updateNotes(noteList.sortedBy { it.updateTime })
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.getNotes()
     }
 
     private fun goToNoteDetails(id: Long = 0L) {
